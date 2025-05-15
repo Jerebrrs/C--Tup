c8036ef77cda5a8b4288311058590f18bc3f7587
Kevin Jeremias <kevinbrrs96@gmail.com>
2025-05-15 12:56:03 -0300

    Feat: primer hola mundo en C#

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hola Mundo!");
+    }
+}
+
+
+
+// Console.WriteLine("Hello, World!");
+
